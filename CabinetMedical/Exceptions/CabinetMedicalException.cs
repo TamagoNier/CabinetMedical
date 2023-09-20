e72fd38d87cd1adf56992a9b9c2321cd6e518485
Egor GUTUTUI <egor.gututui@lyc-bonaparte.fr>
2023-09-20 14:31:13 +0200

    Ajout logs en Json

--- CabinetMedical/Exceptions/CabinetMedicalException.cs
+++ CabinetMedical/Exceptions/CabinetMedicalException.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace CabinetMedical.Exceptions
 {
-    class CabinetMedicalException
+    class CabinetMedicalException : Exception
     {
+        public CabinetMedicalException(string message)
+            : base("Erreur de : " + System.Environment.UserName + " le " + DateTime.Now + "\n" + message)
+        {
+
+
+            JObject jsonException = new JObject
+            (
+                new JProperty("Erreur de :", System.Environment.UserName),
+                new JProperty("Date de l'erreur : ", DateTime.Now),
+                new JProperty("Message : ", message)
+            );
+
+            File.WriteAllText(@"E:\GOGOR\bloc 2\CSharp Deuxieme Annee\CabinetMedical\CabinetMedical\logs.json", JsonConvert.SerializeObject(jsonException));
+
+        }
     }
-}
